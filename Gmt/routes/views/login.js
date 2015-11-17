83559f6e37afaa146278af13bf0cf361ebb0f918
Manjeet Kantak <manjeetkantak@ymail.com>
2015-11-17 17:50:39 +0530

    Login

--- Gmt/routes/views/login.js
+++ Gmt/routes/views/login.js
@@ -3,6 +3,31 @@ exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 	locals.section = 'login';
-  view.query('log',keystone.list('Login').model.find());
-  view.render('login');
-  };
+	locals.validationErrors = {};
+	locals.notLoggedInSuccessfully = false;
+
+	view.on('post', {
+		action: 'login'
+	}, function(next) {
+
+		keystone.list('User').model.findOne({
+			email: req.body.email
+		}).exec(function(err, user) {
+			if (user) {
+				user._.password.compare(req.body.password, function(err, isMatch) {
+					if (!err && isMatch) {
+						res.redirect('/profile')
+						console.log("Success");
+					} else {
+						res.redirect('/login');
+						locals.notLoggedInSuccessfully = true;
+					}
+				});
+			} else {
+				res.redirect('/login')
+				locals.notLoggedInSuccessfully = true;
+			}
+		});
+	});
+	view.render('login');
+}
